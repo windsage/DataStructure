f7858d1ce39b01da60139179a846fe98089705f0
Jeff <chaoxu30@crediteae.cn>
2017-12-08 18:35:54 +0800

    AVL树

--- src/org/jeff/datastructure/binarytree/AVLTree.java
+++ src/org/jeff/datastructure/binarytree/AVLTree.java
@@ -4,6 +4,7 @@ public class AVLTree<T extends Comparable<T>> {
 
     /**
      * 二叉平衡树结点
+     *
      * @param <T>
      */
     private class AVLTreeNode<T> {
@@ -18,5 +19,65 @@ public class AVLTree<T extends Comparable<T>> {
             this.height = 0;
             this.key = key;
         }
+    }
+
+    /**
+     * 获取二叉树的高度
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    private int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    /**
+     * LL旋转
+     *
+     * @param k2 最小不平衡树
+     * @return
+     */
+    private AVLTreeNode<T> leftLeftRotation(AVLTreeNode<T> k2) {
+        AVLTreeNode<T> k1;
+        k1 = k2.left;
+        k2.left = k1.right;
+        k1.right = k2;
+        k1.height = max(k1.left.height, k1.right.height) + 1;
+        k2.height = max(k2.left.height, k2.right.height) + 1;
+        return k1;
+    }
+
+    /**
+     * LR旋转
+     *
+     * @param k3
+     * @return
+     */
+    private AVLTreeNode<T> leftRightRotation(AVLTreeNode<T> k3) {
+        AVLTreeNode<T> k1 = k3.left;
+        AVLTreeNode<T> k2 = k1.right;
+        k1.left = k2.left;
+        k2.left = k1;
+        k3.left = k2.right;
+        k2.right = k3;
+        return k2;
+    }
+
+
+    /**
+     * RR旋转
+     *
+     * @param k2
+     * @return
+     */
+    private AVLTreeNode<T> rightRightRotation(AVLTreeNode<T> k2) {
+        AVLTreeNode<T> k1;
+        k1 = k2.right;
+        k2.right = k1.left;
+        k1.left = k2.right;
+        k1.height = max(k1.left.height, k1.right.height) + 1;
+        k2.height = max(k2.left.height, k2.right.height) + 1;
+        return k1;
     }
 }
