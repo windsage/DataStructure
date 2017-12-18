511d2ec14ac50c1dfdc2f8ea03e45eaca81338a0
Jeff <chaoxu30@creditease.cn>
2017-12-19 07:27:26 +0800

    完善左倾堆

--- src/org/jeff/datastructure/heap/LeftistHeap.java
+++ src/org/jeff/datastructure/heap/LeftistHeap.java
@@ -25,6 +25,52 @@ public class LeftistHeap<T extends Comparable<T>> {
 
     private LeftistNode<T> mRoot;
 
+
+    /*
+     * 前序遍历"左倾堆"
+     */
+    private void preOrder(LeftistNode<T> heap) {
+        if(heap != null) {
+            System.out.print(heap.key+" ");
+            preOrder(heap.left);
+            preOrder(heap.right);
+        }
+    }
+
+    public void preOrder() {
+        preOrder(mRoot);
+    }
+
+    /*
+     * 中序遍历"左倾堆"
+     */
+    private void inOrder(LeftistNode<T> heap) {
+        if(heap != null) {
+            inOrder(heap.left);
+            System.out.print(heap.key+" ");
+            inOrder(heap.right);
+        }
+    }
+
+    public void inOrder() {
+        inOrder(mRoot);
+    }
+
+    /*
+     * 后序遍历"左倾堆"
+     */
+    private void postOrder(LeftistNode<T> heap) {
+        if(heap != null)
+        {
+            postOrder(heap.left);
+            postOrder(heap.right);
+            System.out.print(heap.key+" ");
+        }
+    }
+
+    public void postOrder() {
+        postOrder(mRoot);
+    }
     /**
      * 合并
      *
@@ -32,7 +78,7 @@ public class LeftistHeap<T extends Comparable<T>> {
      * @param y
      * @return
      */
-    private LeftistNode<T> merget(LeftistNode<T> x, LeftistNode<T> y) {
+    private LeftistNode<T> merge(LeftistNode<T> x, LeftistNode<T> y) {
         if (x == null)
             return y;
         if (y == null)
@@ -43,7 +89,7 @@ public class LeftistHeap<T extends Comparable<T>> {
             y = x;
             x = temp;
         }
-        x.right = merget(x.right, y);
+        x.right = merge(x.right, y);
         if (x.left == null || x.left.npl < x.right.npl) {
             LeftistNode<T> tmp = x.right;
             x.right = x.left;
@@ -59,6 +105,87 @@ public class LeftistHeap<T extends Comparable<T>> {
 
 
     public void merge(LeftistHeap<T> other) {
-        merget(mRoot, other.mRoot);
+        merge(mRoot, other.mRoot);
+    }
+
+    /*
+     * 新建结点(key)，并将其插入到左倾堆中
+     *
+     * 参数说明：
+     *     key 插入结点的键值
+     */
+    public void insert(T key) {
+        LeftistNode<T> node = new LeftistNode<T>(key,null,null);
+
+        // 如果新建结点失败，则返回。
+        if (node != null)
+            this.mRoot = merge(this.mRoot, node);
+    }
+
+    /*
+     * 删除根结点
+     *
+     * 返回值：
+     *     返回被删除的节点的键值
+     */
+    public T remove() {
+        if (this.mRoot == null)
+            return null;
+
+        T key = this.mRoot.key;
+        LeftistNode<T> l = this.mRoot.left;
+        LeftistNode<T> r = this.mRoot.right;
+
+        this.mRoot = null;          // 删除根节点
+        this.mRoot = merge(l, r);   // 合并左右子树
+
+        return key;
+    }
+
+    /*
+     * 销毁左倾堆
+     */
+    private void destroy(LeftistNode<T> heap) {
+        if (heap==null)
+            return ;
+
+        if (heap.left != null)
+            destroy(heap.left);
+        if (heap.right != null)
+            destroy(heap.right);
+
+        heap=null;
+    }
+
+    public void clear() {
+        destroy(mRoot);
+        mRoot = null;
+    }
+
+    /*
+     * 打印"左倾堆"
+     *
+     * key        -- 节点的键值
+     * direction  --  0，表示该节点是根节点;
+     *               -1，表示该节点是它的父结点的左孩子;
+     *                1，表示该节点是它的父结点的右孩子。
+     */
+    private void print(LeftistNode<T> heap, T key, int direction) {
+
+        if(heap != null) {
+
+            if(direction==0)    // heap是根节点
+                System.out.printf("%2d(%d) is root\n", heap.key, heap.npl);
+            else                // heap是分支节点
+                System.out.printf("%2d(%d) is %2d's %6s child\n", heap.key, heap.npl, key, direction==1?"right" : "left");
+
+            print(heap.left, heap.key, -1);
+            print(heap.right,heap.key,  1);
+        }
+    }
+
+    public void print() {
+        if (mRoot != null)
+            print(mRoot, mRoot.key, 0);
     }
 }
